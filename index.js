a6f44a3fbb7085bb3623821ff10c19076b7c14f2
Petr Valenta <petr.valenta.92@gmail.com>
2023-11-05 22:27:48 +0100

    Get movies' IDs

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 // Data source https://www.omdbapi.com/
 // get API key from https://www.omdbapi.com/apikey.aspx
+// apiKey = 4530f1ff
 
-// !Delete later!
-const apiKey = "4530f1ff";
+function getIDArray(movie) {
+  fetch(
+    `https://www.omdbapi.com/?apikey=4530f1ff&s=${movie}&type=movie&plot=full`
+  )
+    .then((respo) => respo.json())
+    .then((data) => {
+      const IDArray = data.Search.map((movie) => movie.imdbID);
+      console.log(IDArray); // ?debugging
+      return IDArray;
+    });
+}
 
 document.getElementById("submit").addEventListener("click", (e) => {
   e.preventDefault();
-  const demandMovie = document.getElementById("demand-movie").value;
-
-  fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${demandMovie}`)
-    .then((respo) => respo.json())
-    .then((data) => {
-      console.log(data.Search);
-    });
+  const demandMovie = document.getElementById("search-movie").value;
+  getIDArray(demandMovie);
 });
